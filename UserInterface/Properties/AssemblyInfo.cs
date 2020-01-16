f516f39c39b4616540db8a411f8b427145e8e655
Jacob Affinito <jaffinito@newrelic.com>
2020-01-16 13:46:41 -0800

    Re-version and clean up.

--- UserInterface/Properties/AssemblyInfo.cs
+++ UserInterface/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("dotNet Agent Configuration Tool")]
 [assembly: AssemblyDescription("Tool for configuring the New Relic .Net Agent")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("New Relic")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("dotNet Agent Configuration Tool")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.10.43.0")]
-[assembly: AssemblyFileVersion("3.10.43.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
